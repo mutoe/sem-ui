55ddcd1e9fb36ab9e1cc8023c77c7fa72b3afa9d
mutoe <imutoe@gmail.com>
2023-07-02 21:27:01 +0800

    feat: implement variation menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,8 +1,9 @@
 <template>
-  <div role="tablist" class="sem-menu">
+  <div role="tablist" :class="classes">
     <button
       v-for="(item, index) in list"
       :id="id && `${id}-tab-${item.key || index}`"
+      ref="buttonRefs"
       :key="item.key || index"
       :class="['sem-tab', {active: isActive(index)}]"
       role="tab"
@@ -23,6 +24,7 @@
 
 <script lang="ts" setup generic="T extends MenuItem">
 import { computed } from 'vue'
+import pick from 'src/utils/pick'
 
 // TODO: ARIA shortcuts https://www.w3.org/WAI/ARIA/apg/patterns/menubar/
 
@@ -38,11 +40,19 @@ const props = defineProps<{
   list: string[] | T[]
   id?: string
   activatedItem?: KeyOrIndex
+  secondary?: boolean
+  indicator?: boolean
+  tabbed?: boolean
 }>()
 
 const emit = defineEmits<{
   (e: 'click', index: number, key?: string): void
 }>()
+
+const classes = computed(() => [
+  'sem-menu',
+  pick(props, ['secondary', 'indicator', 'tabbed']),
+])
 
 const list = computed(() => props.list.map(item => typeof item === 'string' ? { title: item } : item) as T[])
 
@@ -61,17 +71,19 @@ function isActive (currentIndex: number) {
 .sem-menu {
   display flex
   width 100%
-  min-height rigor(20/7em)
+  min-height rigor(20 / 7em)
   flex-direction row
   border 1px solid $color-border
   border-radius $border-radius
   box-shadow 0 1px 2px 0 $color-light
 
   .sem-tab {
-    padding (6.5/7em) (11/7em)
+    position relative
+    padding (6.5 / 7em) (11 / 7em)
     border none
     border-right 1px solid $color-border
     background transparent
+
     cursor pointer
 
     &:hover {
@@ -81,7 +93,99 @@ function isActive (currentIndex: number) {
     &:active
     &.active {
       background $color-active
+
+      ^[0]:not(.secondary,.tabbed).indicator ^[1..-2].active::after {
+        position absolute
+        bottom 0
+        left 50%
+        width 8px
+        height 8px
+        border-right 1px solid $color-border
+        border-bottom 1px solid $color-border
+        background #f2f2f2
+        content ""
+        transform translate(-4px, 4px) rotate(45deg)
+      }
+    }
+  }
+
+  &.secondary {
+    border none
+    box-shadow none
+
+    .sem-tab {
+      border-radius $border-radius
+      border-right none
+      margin-right rigor(4/7em)
+    }
+
+    &.indicator {
+      position relative
+
+      &::before {
+        position absolute
+        right 0
+        bottom 0
+        left 0
+        height 2px
+        background-color $color-border
+        content ''
+      }
+
+      .sem-tab {
+        border-radius 0
+        border-bottom 2px solid transparent
+        margin 0
+
+        &:hover {
+          background none
+        }
+
+        &:active
+        &.active {
+          border-bottom-color $color-placeholder
+          background none
+        }
+
+        &.active {
+          border-bottom-color $color-text
+          font-weight bold
+        }
+      }
+    }
+  }
+
+  &.tabbed {
+    position relative
+    border none
+    box-shadow none
+
+    &::before {
+      position absolute
+      right 0
+      bottom 0
+      left 0
+      height 1px
+      background-color $color-border
+      content ''
+    }
+
+    .sem-tab {
+      border none
+      background none
+      border-top-left-radius $border-radius
+      border-top-right-radius $border-radius
+
+      &.active {
+        border 1px solid $color-border
+        border-bottom-color #fff
+      }
     }
   }
 }
+
+:global(.sem-menu + .sem-segment.attached) {
+  border-top none
+}
+
 </style>
--- src/pages/menu-page.vue
+++ src/pages/menu-page.vue
@@ -6,7 +6,30 @@
       <h3>Basic menu</h3>
 
       <div>
-        <Menu id="basic-menu" :list="list" />
+        <Menu id="basic-menu" activated-item="tab1" :list="list" />
+        <br>
+        <Menu id="basic-indicator-menu" indicator activated-item="tab1" :list="list" />
+      </div>
+    </div>
+
+    <div class="field">
+      <h3>Secondary menu</h3>
+
+      <div>
+        <Menu id="secondary-menu" secondary :activated-item="0" :list="list" />
+        <br>
+        <Menu id="secondary-indicator-menu" indicator secondary activated-item="tab1" :list="list" />
+      </div>
+    </div>
+
+    <div class="field">
+      <h3>Tabbed menu</h3>
+
+      <div>
+        <Menu id="tabbed-menu" tabbed :list="list" activated-item="tab1" />
+        <Segment attached>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci commodi delectus dolores, dolorum eius est fugiat, labore mollitia omnis qui ratione sapiente tempora temporibus vitae. Ad aspernatur eaque minima.
+        </Segment>
       </div>
     </div>
   </section>
@@ -14,6 +37,7 @@
 
 <script lang="ts" setup>
 import Menu from 'src/components/Menu.vue'
+import Segment from 'src/components/Segment.vue'
 
 const list = [
   { title: 'Tab1', key: 'tab1' },
