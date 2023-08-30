0fd3f9db523f336ee78133611bbc8210d66a6a59
mutoe <imutoe@gmail.com>
2023-08-30 21:33:12 +0800

    feat(modal): inside form request feature

--- src/composables/use-form.ts
+++ src/composables/use-form.ts
@@ -3,9 +3,10 @@ import { reactive, ref } from 'vue'
 import type { ValidateResult, ValidationSchema } from '@mutoe/form-validator'
 import { validateForm } from '@mutoe/form-validator'
 
-export default function useForm<F extends Record<keyof F, any>> (formData: F, schema: ValidationSchema<F> = {}) {
+export default function useForm<F extends Record<keyof F, any>> (formData: F, schema: ValidationSchema<F> = {}, api?: (form: F) => void | Promise<void>) {
   const form: Ref<F> = ref(formData) as Ref<F>
   const errors: Ref<ValidateResult<F>> = ref({})
+  const submitting = ref(false)
 
   function isFormValid (): boolean {
     const result = validateForm(form.value, schema)
@@ -13,7 +14,7 @@ export default function useForm<F extends Record<keyof F, any>> (formData: F, sc
     return !Object.values(result).some(Boolean)
   }
 
-  const formBinding = reactive({
+  const formProps = reactive({
     modelValue: form,
     onUpdateModelValue: (newForm: F) => void (form.value = newForm),
     errors,
@@ -21,11 +22,37 @@ export default function useForm<F extends Record<keyof F, any>> (formData: F, sc
     schema,
   })
 
+  function parseErrorResponse (error: any) {
+    if (error?.response?.status !== 400 || !error?.response?.data) return
+    errors.value = JSON.parse(error.response.data)
+  }
+
+  const formEvents = reactive({
+    submit: api
+      ? async (event: Event) => {
+        event.preventDefault()
+        if (!isFormValid()) return
+
+        try {
+          submitting.value = true
+          await api(form.value)
+        } catch (error) {
+          parseErrorResponse(error)
+        } finally {
+          submitting.value = false
+        }
+      }
+      : undefined,
+  })
+
   return {
     form,
     errors,
     schema,
-    formBinding,
+    formProps,
+    formEvents,
     isFormValid,
+    parseErrorResponse,
+    submitting,
   }
 }
